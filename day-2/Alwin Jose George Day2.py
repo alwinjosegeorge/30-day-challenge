a531154d2d29698f7fe373dfc5078ea413338016
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-11-19 10:18:56 +0530

    Alwin Jose George Day2.py

--- day-2/Alwin Jose George Day2.py
+++ day-2/Alwin Jose George Day2.py
@@ -1,13 +1,14 @@
-num1 = 10
-num2 = 5
+# Define variables
+num1 = 8
+num2 = 3
 
-#Perform calculations
+# Perform calculations
 sum_result = num1 + num2
 difference_result = num1 - num2
 product_result = num1 * num2
 quotient_result = num1 / num2
 
-#Display results with labels
+# Display results with labels
 print("The sum is", sum_result)
 print("The difference is", difference_result)
 print("The product is", product_result)
