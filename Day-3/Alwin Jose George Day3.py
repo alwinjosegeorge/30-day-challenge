38fb2dcd9566be1c0f037a9740708cd855592845
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-11-19 10:11:29 +0530

    Add files via upload

---
+++ Day-3/Alwin Jose George Day3.py
@@ -0,0 +1,12 @@
+# Define the variable
+str1= "hello world"
+
+# Extract the word "world" and print it
+str2= str1[6:]
+print(str2)
+
+# Concatenate with "everyone" to form a new phrase
+new_string= str2 + " everyone"
+
+# Display the result with a label
+print("The modified text is:", new_string)
