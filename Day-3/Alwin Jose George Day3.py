fa6779deaf6093196ec1b48ee0c1c8dafcb272ba
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-11-19 10:16:20 +0530

    Alwin Jose George Day3.py

--- Day-3/Alwin Jose George Day3.py
+++ Day-3/Alwin Jose George Day3.py
@@ -1,8 +1,8 @@
 # Define the variable
-str1= "hello world"
+str1= "goodbye world"
 
 # Extract the word "world" and print it
-str2= str1[6:]
+str2= str1[8:]
 print(str2)
 
 # Concatenate with "everyone" to form a new phrase
