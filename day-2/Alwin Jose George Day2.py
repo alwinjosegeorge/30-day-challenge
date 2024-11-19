0e33a35bbf0927dfb7e511b4a8c72f47ee342137
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-11-19 10:10:20 +0530

    Add files via upload

---
+++ day-2/Alwin Jose George Day2.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 5
+
+#Perform calculations
+sum_result = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
+quotient_result = num1 / num2
+
+#Display results with labels
+print("The sum is", sum_result)
+print("The difference is", difference_result)
+print("The product is", product_result)
+print("The quotient is", quotient_result)
